a2bf745d49e9739fb2c2f6fb8eb2daefc129a40a
dennisn <denonjugush@gmail.com>
2019-02-27 08:41:25 +0200

    response contains an \'Item\' object

--- taskapp/gettask.py
+++ taskapp/gettask.py
@@ -4,12 +4,15 @@
 A lambda function to retrieve tasks stored in a dynamoDB
 
 Test item
-{"id": {"S": "1"}}
+{
+"id":"1",
+}
 """
 import boto3
 
-dynamodb = boto3.resource('dynamodb')  # endpoint_url="http://localhost:8000
+dynamodb = boto3.resource('dynamodb')
 client = boto3.client('dynamodb')
+table_name = 'todolist'
 
 def gettask_handler(event, context):
     """
@@ -17,12 +20,14 @@ def gettask_handler(event, context):
 
     input is an object with a task id
     """
+    table = dynamodb.Table(table_name)
     try:
-        resp = client.get_item(
-            TableName='todolist',
+        resp = table.get_item(
+            TableName=table_name,
             Key={
                 "id": event['id']
                 }
             )
+        return resp['Item']
     except Exception as e:
         print("Getting an object from table failed: {}".format(e))
