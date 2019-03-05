477c90623dfa2e6ee2c6e01d9a28b926b7275908
dennisn <denonjugush@gmail.com>
2019-03-05 19:35:01 +0200

    update the updatetask function to return API gateway acceptable object

--- taskapp/updatetask.py
+++ taskapp/updatetask.py
@@ -15,23 +15,28 @@ dynamodb = boto3.resource('dynamodb')
 client = boto3.client('dynamodb')
 table_name = 'todolist'
 
+dynamodb = boto3.resource('dynamodb')
+table_name = 'todolist'
+
 def updateTaskStatus_handler(event, context):
     """
     Update an item (task) in dynamoDB table.
 
-    input is an object with a task id
+    input is an object with a task id and other attributes to be
+    updated.
     """
     table = dynamodb.Table(table_name)
+    key={"id": event["queryStringParameters"]["id"]}
     try:
         resp = table.update_item(
-            TableName=table_name,
-            Key={
-                "id": event['id']
-                },
+            Key=key,
             UpdateExpression='SET done = :val1',
-            ExpressionAttributeValues={
-                ':val1': event["done"]
-                }
+            ExpressionAttributeValues={':val1': event["queryStringParameters"]["done"]}
             )
+        return{
+            "statusCode": 200,
+            "headers": {"Access-Control-Allow-Origin" : "*",},
+            "body": json.dumps(resp)
+        }
     except Exception as e:
         print("Updating a task failed: {}".format(e))
